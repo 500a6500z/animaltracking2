db6bba3c595b3be8da46081af302e0633dafd111
Assaf Vayner <assafvayner@gmail.com>
2019-01-16 09:48:21 -0800

    initial frames and dataset

---
+++ src/aFrame.java
@@ -0,0 +1,18 @@
+public class aFrame {
+
+    public Point getCenter() {
+        return center;
+    }
+
+    Point center;
+    double speed;
+    long time;
+
+
+    public double getDistanceFromCenter(){
+        double dispX = center.getX() - Main.getDisplayWidth()/2;
+        double dispY = center.getY() - Main.getDisplayHeight()/2;
+        return Math.sqrt(dispX*dispX - dispY*dispY);
+    }
+
+}
