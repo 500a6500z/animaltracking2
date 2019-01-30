98aaa2f665b4486d828cd9746b9c6b95ad8ec692
David Zhang <dzdavid133@gmail.com>
2019-01-30 09:43:50 -0800

    added String constructor to Point

--- src/Point.java
+++ src/Point.java
@@ -10,9 +10,23 @@ public class Point {
 
     int r, c;
 
+    /**
+     * Constructs a point given the row and column coordinates
+     * @param row row coordinate
+     * @param col column coordinate
+     */
     public Point(int row, int col) {
         r = row;
         c = col;
+    }
+
+    /**
+     * Constructs a point given a string with comma separated row and column coordinates
+     * @param str String in the format "row,col" where row and col are the coordinates
+     */
+    public Point(String str) {
+        r = Integer.parseInt(str.substring(0, str.indexOf(',')));
+        c = Integer.parseInt(str.substring(str.indexOf(',') + 1));
     }
 
     public double getDistance(Point center) {
