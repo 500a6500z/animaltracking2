9259479ddfb952ed2fb64fac8972a3bcdca5d231
Assaf Vayner <assafvayner@gmail.com>
2019-02-01 09:12:47 -0800

    should work now

--- src/getDataSetFromFile.java
+++ src/getDataSetFromFile.java
@@ -1,6 +1,7 @@
 public class getDataSetFromFile {
 
     public static void main(String[] args) {
-        DataSet d = new DataSet("MouseName");
+        DataSet d = new DataSet("MouseData");
+        d.print();
     }
 }
