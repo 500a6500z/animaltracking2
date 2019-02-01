49d600ca38e1a1ebee1f01d5d417a0269279bb64
David Zhang <dzdavid133@gmail.com>
2019-02-01 09:53:18 -0800

    did methods

--- src/getDataSetFromFile.java
+++ src/getDataSetFromFile.java
@@ -2,6 +2,16 @@ public class getDataSetFromFile {
 
     public static void main(String[] args) {
         DataSet d = new DataSet("MouseData.csv");
-        d.print();
+        System.out.println("total distance traveled: " + d.getTotalDistanceTraveled(0, 399));
+        System.out.println("time spent moving slower than 3 cm/s: " + d.getTimeSpentAtSpeed(1.5, 1.5));
+        double max = 0;
+        for(int i = 0; i < d.getData().size(); i++) {
+            if(max < d.getMouseSpeed(i)) {
+                max = d.getMouseSpeed(i);
+            }
+        }
+        System.out.println("max speed: " + max);
+        System.out.println("time spent at 80%: " + d.getTimeSpentAtSpeed(.9 * max, .1 * max));
+
     }
 }
